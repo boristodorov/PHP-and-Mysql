4e9085ca0c6473c0dbe8c0b904e4b88eed3b7816
Boris Todorov <1boris.todorov9@gmail.com>
2016-03-15 13:40:56 +0100

    introducing with array

--- 1.Uvod_u_PHP/3.array.php
+++ 1.Uvod_u_PHP/3.array.php
@@ -5,6 +5,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * Kod PHP-a pomocu array-a mozemo da deklarisemo promenljivu i u njoj da cuvamo podatke,
+ * koji su razlicitog tipa, koncepcija je prosta za svaki kljuc imamo vrednost,
+ * samo sto u kompjuterskim jezicima kljuc uvek pocinje od 0 a ne od 1.
+ */
 $myArray = array('Boris', 'Todorov', 'Dimitrovgrad');
 echo $myArray[0]; //Boris
 echo $myArray[1]; //Todorov
@@ -14,30 +19,35 @@ $myArray2 = array ('Boris' , 123 , true);
 $myArray2[2] = FALSE;
 var_dump($myArray2[2]); // false
 
+//Mozemo i da ne zadajemo kljuc, vec php ce sam odrediti kljuc
 $myArray3 [] = 'Boris';
 $myArray3 [] = 12;
 $myArray3 [] = true;
         
 var_dump($myArray3); // array(3) {[0]string=>"Boris" [1]int(12) [2]bool=>true}
 
+//mozemo sami proizvoljno zadati kljuc
 $myArray4 [4] = 12;
 $myArray4 [3] = 'Boris';
 $myArray4 [8] = true;
 var_dump($myArray4); // array(3) [ [4]int(12) [3]string(5)"Boris" [8]bool=> true}
 
+//mozemo zadati kljuc i kao string
 $myArray5 ['username'] = 'Boris';
 $myArray5 [2] = 23;
 $myArray5 [8] = true;
 
 echo $myArray5['username']; // Boris
 
-
+//mozemo u jednom nizu zadani drugi niz, u drugi niz od nekih vrednosti zadati treci, 
+//ali vise od 4 5 ugracenja niza nisu preporucljivi
 $myArray6 [3] = 234;
 $myArray6 ['username'] = 'Boris';
 $myArray6 ['a'] = array(2 , 3 , 5);
 
 print_r($myArray6); 
-/*Array
+/*
+ * Array
 (
     [3] => 234
     [username] => Boris
